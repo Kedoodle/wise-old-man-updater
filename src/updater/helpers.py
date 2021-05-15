e6852b6e728814462f354ab35418fdc8c4a9c7a4
Keddy Shen <kedd.shen@gmail.com>
2021-05-16 00:14:16 +1000

    Add type hints

--- src/updater/helpers.py
+++ src/updater/helpers.py
@@ -1,20 +1,20 @@
 from datetime import datetime
 
 
-def filter_active_competitions(competitions):
+def filter_active_competitions(competitions: list) -> list[dict]:
     return [c for c in competitions if _is_active(c)]
 
 
-def _is_active(competition):
+def _is_active(competition: dict) -> bool:
     fmt = '%Y-%m-%dT%H:%M:%S.%fZ'
     start_time = datetime.strptime(competition['startsAt'], fmt)
     end_time = datetime.strptime(competition['endsAt'], fmt)
     return start_time <= _now() <= end_time
 
 
-def _now():
+def _now() -> datetime:
     return datetime.utcnow()
 
 
-def get_competition_ids(competitions):
+def get_competition_ids(competitions: list) -> list[int]:
     return [c['id'] for c in competitions]
--- src/updater/services.py
+++ src/updater/services.py
@@ -3,13 +3,13 @@ import requests
 from src.updater.config import BASE_URL, GROUP_ID, VERIFICATION_CODE
 
 
-def get_groups_competitions(id=GROUP_ID):
+def get_groups_competitions(id: int = GROUP_ID) -> requests.Response:
     url = f'{BASE_URL}/groups/{id}/competitions'
     response = requests.get(url)
     return response if response.ok else None
 
 
-def post_competitions_update_all(id):
+def post_competitions_update_all(id: int) -> requests.Response:
     url = f'{BASE_URL}/competitions/{id}/update-all'
     body = {'verificationCode': VERIFICATION_CODE}
     response = requests.post(url, json=body)
