e1da592e1db967785620751c58f8426f213108c9
Keddy Shen <kedd.shen@gmail.com>
2021-05-28 01:54:22 +1000

    Update players individually synchronously

--- src/updater/__main__.py
+++ src/updater/__main__.py
@@ -1,15 +1,16 @@
-from src.updater.services import get_groups_competitions, post_competitions_update_all
-from src.updater.helpers import filter_active_competitions, get_competition_ids
+from src.updater.services import get_groups_members, post_competitions_update_all, post_players_track
+from src.updater.helpers import filter_active_competitions, get_competition_ids, filter_outdated_players, \
+    get_player_usernames
 
 
 def run():
-    competitions = get_groups_competitions().json()
-    active_competitions = filter_active_competitions(competitions)
-    competition_ids = get_competition_ids(active_competitions)
-    print(f'Updating {len(competition_ids)} competitions...')
-    for competition_id in competition_ids:
-        print(f'Updating competition with id {competition_id}')
-        post_competitions_update_all(competition_id)
+    players = get_groups_members().json()
+    outdated_players = filter_outdated_players(players)
+    usernames = get_player_usernames(outdated_players)
+    print(f'Updating {len(usernames)} players...')
+    for username in usernames:
+        print(f'Updating player with username "{username}"')
+        post_players_track(username)
 
 
 if __name__ == '__main__':
