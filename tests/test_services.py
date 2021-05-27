709c0daa929adc956dbf923288387b77e25a6bcf
Keddy Shen <kedd.shen@gmail.com>
2021-05-28 00:31:06 +1000

    Add function to track player

--- tests/test_services.py
+++ tests/test_services.py
@@ -124,5 +124,73 @@ class TestGetGroupsMembers(unittest.TestCase):
         self.assertIsNone(response)
 
 
+class TestPostPlayersTrack(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._mock_post_patcher = patch('src.updater.services.requests.post')
+        cls._mock_post = cls._mock_post_patcher.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._mock_post_patcher.stop()
+
+    def test_when_response_is_ok(self):
+        player = {
+            "exp": 287438551,
+            "id": 3033,
+            "username": "kedd",
+            "displayName": "Kedd",
+            "type": "regular",
+            "build": "main",
+            "country": "NZ",
+            "flagged": False,
+            "ehp": 1181.2346,
+            "ehb": 67.81755,
+            "ttm": 127.74393,
+            "tt200m": 13781.21645,
+            "lastImportedAt": "2021-05-27T14:15:15.929Z",
+            "lastChangedAt": "2021-05-27T12:34:57.824Z",
+            "registeredAt": "2020-05-02T22:38:09.634Z",
+            "updatedAt": "2021-05-27T14:27:32.334Z",
+            "combatLevel": 123,
+            "latestSnapshot": {
+                "createdAt": "2021-05-27T14:27:32.338Z",
+                "importedAt": None,
+                "overall": {
+                    "rank": 22901,
+                    "experience": 287438551,
+                    "ehp": 1181.2346
+                },
+                "attack": {
+                    "rank": 248931,
+                    "experience": 10644366,
+                    "ehp": 32.7519
+                },
+                "ehp": {
+                    "rank": 11824,
+                    "value": 1181.2346
+                },
+                "ehb": {
+                    "rank": 45665,
+                    "value": 67.81755
+                }
+            }
+        }
+        self._mock_post.return_value = Mock(status_code=200)
+        self._mock_post.return_value.json.return_value = player
+
+        response = post_competitions_update_all(Mock())
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), player)
+
+    def test_when_response_is_not_ok(self):
+        self._mock_post.return_value.ok = False
+
+        response = post_competitions_update_all(Mock())
+
+        self.assertIsNone(response)
+
+
 if __name__ == '__main__':
     unittest.main()
